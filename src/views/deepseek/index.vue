ffbf9bc1175982790095a17758ca3feece0ca398
HuangJiangye <1419210357@qq.com>
2025-10-02 18:57:17 +0800

    fix

--- src/views/deepseek/index.vue
+++ src/views/deepseek/index.vue
@@ -269,7 +269,7 @@ const handleRequest = async () => {
 onMounted(async () => {
   const meta = document.createElement('meta')
   meta.name = 'viewport'
-  meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+  meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover'
   document.head.appendChild(meta)
 
   initSessionList()
